001a93976b5e7bd401ac3369c90c30d55f97be31
Beritha <nberitha78@gmail.com>
2023-12-06 13:27:59 -0800

    function adds all unique integers in a listw

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[])
+    unique_set = set()
+
+    for item in my_list:
+        unique_set.add(item)
+
+    result_sum = sum(unique_set)
+
+    return result_sum
